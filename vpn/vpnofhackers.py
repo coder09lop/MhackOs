9000f950e2cde24b8c96985465a6a034cc7c02ff
coder09lop <omar.grini@icpeschieradelgarda.edu.it>
2024-08-04 19:11:24 +0200

    ver 1.10.2
    MhackOs

---
+++ vpn/vpnofhackers.py
@@ -0,0 +1,73 @@
+import random
+
+
+
+print("benvenuti/bentornato nella vpn degli hackers")
+print("per favore inserisci il tuo username e password")
+username = input("inserisci il tuo username: ")
+if username == "riyadsuper":
+    print("benvenuto" + username)
+    print("inserisci password")
+    password = input()
+    if password == "Omargrini2011@":
+        print("password corretta")
+        print("sei connesso alla vpn degli hacker")
+        print("ora ti daremo l'ip")
+        print("l'ip è")
+        ip = ("192.89.0987", "1892.9303,9393", "9393.9484.9494", "192.9090.8765", "hacking.9090")
+        print(random.choice(ip))
+    else:
+        print("password sbagliata")
+
+
+
+
+else:
+    print("username non trovato")
+    print("sei stato bloccato")
+    print("inserisci codice di sicurezza")
+    codice2317 = input()
+    #primocodice
+    if codice2317 == "159876":
+        print("codice corretto")
+        print("sei stato sbloccato")
+        print("premi invio per cominciare")
+        invio = input()
+        print("benvenuti/bentornato nella vpn degli hackers")
+print("per favore inserisci il tuo username e password")
+username = input("inserisci il tuo username: ")
+if username == "riyadsuper":
+    print("benvenuto" + username)
+    print("inserisci password")
+    password = input()
+    if password == "Omargrini2011@":
+        print("password corretta")
+        print("sei connesso alla vpn degli hacker")
+        print("ora ti daremo l'ip")
+        print("l'ip è")
+        ip = ("192.89.0987", "1892.9303,9393", "9393.9484.9494", "192.9090.8765", "hacking.9090")
+        print(random.choice(ip))
+
+    #seccodice
+    elif codice2317 == "codice2317":
+        print("sei stato sbloccato")
+        print("premi invio per cominciare")
+        invio2 = input()
+        print("benvenuti/bentornato nella vpn degli hackers")
+print("per favore inserisci il tuo username e password")
+username = input("inserisci il tuo username: ")
+if username == "riyadsuper":
+    print("benvenuto" + username)
+    print("inserisci password")
+    password = input()
+    if password == "Omargrini2011@":
+        print("password corretta")
+        print("sei connesso alla vpn degli hacker")
+        print("ora ti daremo l'ip")
+        print("l'ip è")
+        ip = ("192.89.0987", "1892.9303,9393", "9393.9484.9494", "192.9090.8765", "hacking.9090")
+        print(random.choice(ip))
+
+#default
+else:
+    print("username non trovato")
